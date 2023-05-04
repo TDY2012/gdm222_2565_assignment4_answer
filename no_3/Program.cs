34d2ef76d9f0a65a288ad7e4ce42488e003107a4
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2023-05-04 15:09:36 +0700

    feat: Added solution for assignment4 no3.

---
+++ no_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int nodeCount = int.Parse(Console.ReadLine());
+
+        Graph<int> g = new Graph<int>();
+
+        for(int i=0; i<nodeCount; i++)
+        {
+            g.AddNode(i);
+        }
+
+        int srcIndex, dstIndex;
+        while(true)
+        {
+            srcIndex = int.Parse(Console.ReadLine());
+            if(srcIndex < 0 || srcIndex >= nodeCount)
+            {
+                break;
+            }
+
+            dstIndex = int.Parse(Console.ReadLine());
+            if(dstIndex < 0 || dstIndex >= nodeCount)
+            {
+                break;
+            }
+
+            g.AddEdge(srcIndex, dstIndex);
+        }
+
+        int srcIndexCheck = int.Parse(Console.ReadLine());
+        int dstIndexCheck = int.Parse(Console.ReadLine());
+
+        if(g.IsReachable(srcIndexCheck, dstIndexCheck))
+        {
+            Console.WriteLine("Reachable");
+        }
+        else
+        {
+            Console.WriteLine("Unreachable");
+        }
+    }
+}
